c7de9959755bdf19b6a66d827d142e7e8d4fa15c
lifeakraquel <raquel@lifeak.com>
2016-06-01 10:51:35 +0200

    a

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+if ('production' === process.env.NODE_ENV)
+    require('newrelic');
+
+const PORT = process.env.PORT || 3333;
+
+const os = require('os');
+const express = require('express');
+const RoutesConfig = require('server/config/routes.conf');
+const DBConfig = require('server/config/db.conf');
+const Routes = require('server/routes/index');
+
+const app = express();
+const server = app.listen(PORT);
+
+RoutesConfig.init(app, express);
+DBConfig.init();
+Routes.init(app, express.Router());
+console.log(`up and running @: ${os.hostname()} on port: ${PORT}`);
+console.log(`enviroment: ${process.env.NODE_ENV}`);
