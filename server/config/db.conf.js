0d98bb2de0da17c0c9b05be9d9ca2a3facdd189b
lifeakraquel <raquel@lifeak.com>
2016-06-14 10:11:03 +0200

    mongouri

--- server/config/db.conf.js
+++ server/config/db.conf.js
@@ -5,7 +5,7 @@ const dbConst = require('../constants/db.json');
 
 module.exports = class DBConfig {
     static init() {
-      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOLAB_URI
+      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGODB_URI
                                                           : dbConst.localhost;                                            
       mongoose.connect(URL);
       mongoose.connection.on('error', console.error.bind(console, 'An error ocurred with the DB connection: '));
