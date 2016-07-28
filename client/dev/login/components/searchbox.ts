f8a76177b1e76e72b68262ce69c5bfcb09c9a34a
lifeakraquel <raquel@lifeak.com>
2016-07-28 14:49:15 +0200

    añadiendo pipes1.1

--- client/dev/login/components/searchbox.ts
+++ client/dev/login/components/searchbox.ts
@@ -2,7 +2,6 @@ import {Component , Output, EventEmitter} from "angular2/core";
 @Component({
 	selector:'search-box',
 	template:`<div>
-			<label>Búsqueda por nombre o ciudad:</label>
 			<input #input type="text" (input)="update.emit(input.value)">
 			</div>`
 })
